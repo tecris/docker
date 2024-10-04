2c0829d5aa3c0a752380b69a02eaec1d7f25c4d9
tecristea <tecristea@gmail.com>
2024-10-04 16:34:11 +0300

    [ssh-ca] ed25519

--- ssh-ca/run.sh
+++ ssh-ca/run.sh
@@ -6,8 +6,8 @@ TAG=23.05.16
 docker volume create --name volume_zero
 docker run -v volume_zero:/keys org.tecris/ssh:${TAG} rm -rf /keys/*
 
-docker run -v volume_zero:/keys org.tecris/ssh:${TAG} ssh-keygen -b 4096 -t rsa -f /keys/user_ca -C "CA key for user" -q -N ""
-docker run -v volume_zero:/keys org.tecris/ssh:${TAG} ssh-keygen -b 4096 -t rsa -f /keys/host_ca -C "CA key for host" -q -N ""
+docker run -v volume_zero:/keys org.tecris/ssh:${TAG} ssh-keygen -t ed25519 -f /keys/user_ca -C "CA key for user" -q -N ""
+docker run -v volume_zero:/keys org.tecris/ssh:${TAG} ssh-keygen -t ed25519 -f /keys/host_ca -C "CA key for host" -q -N ""
 docker run -v volume_zero:/keys org.tecris/ssh:${TAG} ls -l /keys
 
 docker run --name volume_helper -d -v volume_zero:/keys busybox:1.37 true
